3b13fe2ac604cf14976c8247d982b49efe7a9e05
himalayan-sanjeev <sanjeevpoudel111@gmail.com>
2022-10-17 15:50:27 +0545

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+  User.create(
+      {
+        first_name: "himalayan#{i + 1}",
+        last_name: "sanjeev#{i + 1}",
+        address_city: "Rainas#{i + 1}",
+        address_state: "Gandaki#{i + 1}",
+        mobile_number: "980000000#{i + 1}",
+        email: "h.sanjeev#{i + 1}@gmail.com",
+        date_of_birth: Date.today - (i+1).years
+      }
+    )
+end
